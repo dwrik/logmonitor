97d88ff17e592c2fb6b218590ec6461fa2c733bd
Wrik Das <wrikdas22@gmail.com>
2024-06-28 00:04:21 +0530

    added entities

---
+++ src/main/java/org/dwrik/logmonitor/entity/Log.java
@@ -0,0 +1,25 @@
+package org.dwrik.logmonitor.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Log {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "log message cannot be blank")
+	private String message;
+
+}
