107b6645ca7dd9a63ad69051e36cc3cff10a15bb
Wrik Das <wrikdas22@gmail.com>
2024-06-27 14:06:51 +0530

    initial commit

---
+++ src/main/java/org/dwrik/logmonitor/LogmonitorApplication.java
@@ -0,0 +1,13 @@
+package org.dwrik.logmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogmonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dwrik/logmonitor/LogmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dwrik.logmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogmonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
